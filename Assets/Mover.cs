3bd5f914bc4f919b88acc7e6101b9b2cf4764eaa
mezz <mezzanine510@gmail.com>
2019-08-19 16:22:27 -0700

    added pathfinding

---
+++ Assets/Mover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    private NavMeshAgent navMeshAgent;
+
+    void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.destination = target.position;
+    }
+}
