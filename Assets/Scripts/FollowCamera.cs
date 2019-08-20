cb076dc72d3cab3bd38e55eba2b3bf1da56fd69a
Mezz <mezzanine510@gmail.com>
2019-08-20 04:29:47 -0700

    Added trees, flora, and rocks; fixed lighting issues; created all new prefabs with new materials

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    // Camera camera;
+    
+    void Awake()
+    {
+        // camera = Camera.main;
+    }
+
+    void Update()
+    {
+        // Camera.main.transform.position = new Vector3(
+        //     target.transform.position.x,
+        //     camera.transform.position.y,
+        //     target.transform.position.z
+        // );
+        transform.position = target.transform.position;
+    }
+}
