b82f455ee994bc9662b4ff60788cbdfa6734da86
Mezz <mezzanine510@gmail.com>
2019-08-26 16:23:51 -0700

    Enemy prefabs reworked; initial AI script implemented: enemies can now detect player in range

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f;
+
+        // GameObject target;
+
+        private void Update() {
+            GameObject player = GameObject.FindWithTag("Player");
+            if (player == null) return;
+
+            if (TargetInRange(player))
+            {
+                print("Chasing " + player.name + "!!!");
+            }
+        }
+
+        private bool TargetInRange(GameObject target)
+        {
+            if (DistanceToTargetSquared(target) < ChaseDistanceSquared()) return true;
+            return false;
+        }
+
+        private float DistanceToTargetSquared(GameObject target)
+        {
+            return (transform.position - target.transform.position).sqrMagnitude;
+        }
+
+        private float ChaseDistanceSquared()
+        {
+            return chaseDistance * chaseDistance;
+        }
+    }
+
+}
