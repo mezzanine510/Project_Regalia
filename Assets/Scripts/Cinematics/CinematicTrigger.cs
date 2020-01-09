2c4dc52f47f132194a0476644d251b081418ff6f
Mezz <mezzanine510@gmail.com>
2020-01-09 01:02:50 -0800

    Added collider that sets up intro sequence on player collision

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other) {
+            if (other.gameObject.tag == "Player")
+            {
+                GetComponent<PlayableDirector>().Play();
+                gameObject.GetComponent<BoxCollider>().isTrigger = false;
+            }
+        }
+    }
+}
