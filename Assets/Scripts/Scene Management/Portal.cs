76a1a5343d4eea2898cc5b6b4516964aa446e4e4
Mezz <mezzanine510@gmail.com>
2020-01-29 16:55:51 -0800

    Added portal between scene 1 and 0; Added spawn point functionality inside transition coroutine

--- Assets/Scripts/Scene Management/Portal.cs
+++ Assets/Scripts/Scene Management/Portal.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
@@ -7,6 +8,7 @@ namespace RPG.SceneManagement
     public class Portal : MonoBehaviour
     {
         [SerializeField] int sceneToLoad;
+        [SerializeField] Transform spawnPoint;
 
         private void OnTriggerEnter(Collider other)
         {
@@ -18,11 +20,42 @@ namespace RPG.SceneManagement
 
         private IEnumerator Transition()
         {
-            // SceneManager.LoadSceneAsync(sceneToLoad);
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
-            print("Scene loaded");
+            print("THIS: " + this);
+            print("gameObject: " + gameObject);
+            
+            Portal targetPortal = GetOtherPortal();
+            print("targetPortal: " + targetPortal);
+            print("targetPortal.spawnPoint: " + targetPortal.spawnPoint);
+            UpdatePlayer(targetPortal);
+
             Destroy(gameObject);
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this)
+                {
+                    continue;
+                };
+                return portal;
+            }
+
+            // if no portal found
+            print("Could not find Portal.");
+            return null;
+        }
+
+        private void UpdatePlayer(Portal targetPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(targetPortal.spawnPoint.position);
+            // player.transform.position = targetPortal.spawnPoint.position;
+            // player.transform.rotation = targetPortal.spawnPoint.rotation;
+            // player.GetComponent<NavMeshAgent>().enabled = true;
         }
     }
 }
