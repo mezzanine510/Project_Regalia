61bd6b518a1d1d79221a104d6a56f85fcdd5ed62
Mezz <mezzanine510@gmail.com>
2020-02-09 20:58:37 -0800

    Added SearializableVector3 to saving system; implemented basic position save and load with S and L keys

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
 using RPG.Core;
+using RPG.Saving;
 
 namespace RPG.Movement
 {
-    public class Mover : MonoBehaviour, IAction
+    public class Mover : MonoBehaviour, IAction, ISaveable
     {
         private ActionScheduler actionScheduler;
         private Animator animator;
@@ -54,6 +55,19 @@ namespace RPG.Movement
         {
             navMeshAgent.destination = transform.position;
             navMeshAgent.isStopped = true;
+        }
+
+        public object CaptureState()
+        {
+            return new SerializableVector3(transform.position);
+        }
+
+        public void RestoreState(object state)
+        {
+            SerializableVector3 position = (SerializableVector3)state;
+            GetComponent<NavMeshAgent>().enabled = false;
+            transform.position = position.ToVector();
+            GetComponent<NavMeshAgent>().enabled = true;
         }
     }
 }
