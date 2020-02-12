4bab5ba58355e43cb71aeb62ba20e4c23edb8a8c
Mezz <mezzanine510@gmail.com>
2020-02-12 01:46:18 -0800

    Added MoverSaveData struct to Mover.cs to save position and rotation data

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -58,21 +58,39 @@ namespace RPG.Movement
             navMeshAgent.isStopped = true;
         }
 
+        [System.Serializable] // make sure to serialize the appropriate public fields
+        struct MoverSaveData
+        {
+            public SerializableVector3 position;
+            public SerializableVector3 rotation;
+        }
+
         public object CaptureState()
         {
-            Dictionary<string, object> data = new Dictionary<string, object>();
-            data["position"] = new SerializableVector3(transform.position);
-            data["rotation"] = new SerializableVector3(transform.eulerAngles);
+            MoverSaveData data = new MoverSaveData();
+            data.position = new SerializableVector3(transform.position);
+            data.rotation = new SerializableVector3(transform.eulerAngles);
             return data;
+
+            // initial method
+            // Dictionary<string, object> data = new Dictionary<string, object>();
+            // data["position"] = new SerializableVector3(transform.position);
+            // data["rotation"] = new SerializableVector3(transform.eulerAngles);
+            // return data;
         }
 
         public void RestoreState(object state)
         {
-            Dictionary<string, object> data = (Dictionary<string, object>)state;
+            MoverSaveData data = (MoverSaveData)state;
             GetComponent<NavMeshAgent>().enabled = false;
-            transform.position = ((SerializableVector3)data["position"]).ToVector();
-            transform.eulerAngles = ((SerializableVector3)data["rotation"]).ToVector();
+            transform.position = data.position.ToVector();
+            transform.eulerAngles = data.rotation.ToVector();
             GetComponent<NavMeshAgent>().enabled = true;
+            // Dictionary<string, object> data = (Dictionary<string, object>)state;
+            // GetComponent<NavMeshAgent>().enabled = false;
+            // transform.position = ((SerializableVector3)data["position"]).ToVector();
+            // transform.eulerAngles = ((SerializableVector3)data["rotation"]).ToVector();
+            // GetComponent<NavMeshAgent>().enabled = true;
         }
     }
 }
